d1273911655bf22e6e4fd21816aa66ca0173af0a
Simmo Saan <simmo.saan@gmail.com>
2018-11-28 17:58:56 +0200

    Add SExp toString methods

--- src/main/scala/eu/sim642/algosmt/smtlib/SExp.scala
+++ src/main/scala/eu/sim642/algosmt/smtlib/SExp.scala
@@ -2,8 +2,13 @@ package eu.sim642.algosmt.smtlib
 
 sealed trait SExp
 
-case class Atom(str: String) extends SExp
-case class Compound(sexps: SExp*) extends SExp
+case class Atom(str: String) extends SExp {
+  override def toString: String = str
+}
+
+case class Compound(sexps: SExp*) extends SExp {
+  override def toString: String = sexps.mkString("(", " ", ")")
+}
 
 object Application {
   def apply(func: String, args: SExp*): SExp = Compound(Atom(func) +: args: _*)
