2eae924aa72b0f6141e3a8731ca1e4a52b843d8f
Simmo Saan <simmo.saan@gmail.com>
2018-11-30 12:28:57 +0200

    Add equivalent IDL constraint forms

--- src/main/scala/eu/sim642/algosmt/idl/IDLConstraintBExpParser.scala
+++ src/main/scala/eu/sim642/algosmt/idl/IDLConstraintBExpParser.scala
@@ -14,19 +14,29 @@ object IDLConstraintBExpParser extends BExpParser[Constraint[String]] {
     }
   }
 
+  object OperatorConstraint {
+    def unapply(sexp: SExp): Option[(String, String, String, Int)] = sexp match {
+      case Application(op, Application("-", Atom(x), Atom(y)), IntAtom(n)) => Some((op, x, y, n)) // x - y OP n
+      case Application(op, Atom(x), Application("+", Atom(y), IntAtom(n))) => Some((op, x, y, n)) // x OP y + n -> x - y OP n
+      case Application(op, Atom(x), Application("+", IntAtom(n), Atom(y))) => Some((op, x, y, n)) // x OP n + y -> x - y OP n
+      case Application(op, Application("+", Atom(x), IntAtom(n)), Atom(y)) => Some((op, x, y, -n)) // x + n OP y -> x - y OP -n
+      case Application(op, Application("+", IntAtom(n), Atom(x)), Atom(y)) => Some((op, x, y, -n)) // n + x OP y -> x - y OP -n
+      case _ => None
+    }
+  }
+
   override def fromSExp(sexp: SExp): BExp[Constraint[String]] = sexp match {
-    case Application("<=", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+    case OperatorConstraint("<=", x, y, n) =>
       Var(Constraint(x, y, n)) // x - y <= n
-    case Application("<", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+    case OperatorConstraint("<", x, y, n) =>
       Var(Constraint(x, y, n - 1)) // x - y < n -> x - y <= n - 1
-    case Application(">=", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+    case OperatorConstraint(">=", x, y, n) =>
       Var(Constraint(y, x, -n)) // x - y >= n -> y - x <= -n
-    case Application(">", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+    case OperatorConstraint(">", x, y, n) =>
       Var(Constraint(y, x, -n - 1)) // x - y > n -> y - x < -n -> y - x <= -n - 1
-    case Application("=", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+    case OperatorConstraint("=", x, y, n) =>
       And(Var(Constraint(x, y, n)), Var(Constraint(y, x, -n))) // x - y = n -> x - y <= n && x - y >= n -> x - y <= n && y - x <= -n
-    case Application("distinct", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+    case OperatorConstraint("distinct", x, y, n) =>
       Or(Var(Constraint(x, y, n - 1)), Var(Constraint(y, x, -n - 1))) // x - y ≠ n -> x - y < n || x - y > n -> x - y <= n - 1 || y - x <= -n - 1
-    // TODO: add equivalent forms
   }
 }
