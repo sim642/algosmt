6143e1ff7b74f3e68b4d4dfe8a18f07951666fba
Simmo Saan <simmo.saan@gmail.com>
2018-11-30 12:13:50 +0200

    Add different IDL operator forms

--- src/main/scala/eu/sim642/algosmt/idl/IDLConstraintBExpParser.scala
+++ src/main/scala/eu/sim642/algosmt/idl/IDLConstraintBExpParser.scala
@@ -1,6 +1,6 @@
 package eu.sim642.algosmt.idl
 
-import eu.sim642.algosmt.bool.{BExp, BExpParser, Var}
+import eu.sim642.algosmt.bool._
 import eu.sim642.algosmt.idl.IDL.Constraint
 import eu.sim642.algosmt.smtlib.{Application, Atom, SExp}
 
@@ -15,7 +15,18 @@ object IDLConstraintBExpParser extends BExpParser[Constraint[String]] {
   }
 
   override def fromSExp(sexp: SExp): BExp[Constraint[String]] = sexp match {
-    case Application("<=", Application("-", Atom(x), Atom(y)), IntAtom(n)) => Var(Constraint(x, y, n))
+    case Application("<=", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+      Var(Constraint(x, y, n)) // x - y <= n
+    case Application("<", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+      Var(Constraint(x, y, n - 1)) // x - y < n -> x - y <= n - 1
+    case Application(">=", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+      Var(Constraint(y, x, -n)) // x - y >= n -> y - x <= -n
+    case Application(">", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+      Var(Constraint(y, x, -n - 1)) // x - y > n -> y - x < -n -> y - x <= -n - 1
+    case Application("=", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+      And(Var(Constraint(x, y, n)), Var(Constraint(y, x, -n))) // x - y = n -> x - y <= n && x - y >= n -> x - y <= n && y - x <= -n
+    case Application("distinct", Application("-", Atom(x), Atom(y)), IntAtom(n)) =>
+      Or(Var(Constraint(x, y, n - 1)), Var(Constraint(y, x, -n - 1))) // x - y ≠ n -> x - y < n || x - y > n -> x - y <= n - 1 || y - x <= -n - 1
     // TODO: add equivalent forms
   }
 }
