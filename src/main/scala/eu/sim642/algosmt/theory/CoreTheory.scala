7799fa4cbe19f71a5c94fb65905dcc18104f8b4b
Simmo Saan <simmo.saan@gmail.com>
2018-12-18 19:19:08 +0200

    Add xor operation to core theory

--- src/main/scala/eu/sim642/algosmt/theory/CoreTheory.scala
+++ src/main/scala/eu/sim642/algosmt/theory/CoreTheory.scala
@@ -1,7 +1,7 @@
 package eu.sim642.algosmt.theory
 
 object CoreTheory extends Theory {
-  override def leftAssocFuncs: Set[String] = Set("and", "or")
+  override def leftAssocFuncs: Set[String] = Set("and", "or", "xor")
   override def chainableFuncs: Set[String] = Set("=")
   override def pairwiseFuncs: Set[String] = Set("distinct")
 }
