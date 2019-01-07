e4798a0c317a8d50be0144d6fd005ddf3f8856f5
Simmo Saan <simmo.saan@gmail.com>
2019-01-07 19:10:05 +0200

    Optimize unit propagation to run multiple times in a row

--- src/main/scala/eu/sim642/algosmt/smt/DPLLSMTSolver.scala
+++ src/main/scala/eu/sim642/algosmt/smt/DPLLSMTSolver.scala
@@ -2,16 +2,18 @@ package eu.sim642.algosmt.smt
 
 import eu.sim642.algosmt.smt.cnf.{CNF, Literal, extractVariables, toLiteral}
 
+import scala.annotation.tailrec
+
 class DPLLSMTSolver[A, B, C](logicSolver: LogicSolver[A, B, C]) extends SMTSolver[A, B, C] {
 
-  override def solve(cnf: CNF[A]): Option[Map[B, C]] = solve(cnf, Set.empty)
+  override def solve(cnf: CNF[A]): Option[Map[B, C]] = solveFull(cnf, Set.empty)
 
   private def propagate(cnf: CNF[A], literal: Literal[A]): CNF[A] = {
     val negLiteral = literal.neg
     cnf.filterNot(_.contains(literal)).map(_ - negLiteral)
   }
 
-  private def solve(cnf: CNF[A], model: Model[A]): Option[Map[B, C]] = {
+  private def solveFull(cnf: CNF[A], model: Model[A]): Option[Map[B, C]] = {
     if (cnf.isEmpty)
       return logicSolver.solve(model)
     else if (cnf.exists(_.isEmpty))
@@ -19,15 +21,33 @@ class DPLLSMTSolver[A, B, C](logicSolver: LogicSolver[A, B, C]) extends SMTSolve
     else if (logicSolver.solve(model).isEmpty)
       return None
 
+    solveUnit(cnf, model)
+  }
+
+  @tailrec
+  private def solveUnit(cnf: CNF[A], model: Model[A], unitCount: Int = 0): Option[Map[B, C]] = {
     // unit propagation
     cnf.find(_.size == 1).map(_.head) match {
       case Some(unitLiteral) =>
-        return solve(propagate(cnf, unitLiteral), model + unitLiteral)
+        solveUnit(propagate(cnf, unitLiteral), model + unitLiteral, unitCount + 1)
       case None =>
-    }
+        if (unitCount > 0) {
+          // TODO: remove duplication with solveFull
+          if (cnf.isEmpty)
+            return logicSolver.solve(model)
+          else if (cnf.exists(_.isEmpty))
+            return None
+          else if (logicSolver.solve(model).isEmpty)
+            return None
+        }
 
+        solveSplit(cnf, model)
+    }
+  }
+
+  private def solveSplit(cnf: CNF[A], model: Model[A]): Option[Map[B, C]] = {
     // splitting
     val variable = extractVariables(cnf).head
-    solve(propagate(cnf, variable), model + variable) orElse solve(propagate(cnf, variable.neg), model + variable.neg)
+    solveFull(propagate(cnf, variable), model + variable) orElse solveFull(propagate(cnf, variable.neg), model + variable.neg)
   }
 }
